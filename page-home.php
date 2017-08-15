b7e7b6b7ce3907614df831196614fdaeb5c3081e
vls48 <vls48@drexel.edu>
2017-08-14 22:09:13 -0400

    ffff

--- page-home.php
+++ page-home.php
@@ -22,6 +22,60 @@
 </style>
 
 			<div id="content">
+				
+					<object class="metals top-right" data="wp-content/themes/bones/library/images/top-right.svg" type="image/svg+xml">
+					</object>
+				
+					<object class="metals top-left" data="wp-content/themes/bones/library/images/top-lefy.svg" type="image/svg+xml">
+					</object>
+				
+
+				
+					<svg class="metals top-right" width="119px" height="89px" viewBox="0 0 119 89" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+					    <!-- Generator: Sketch 46 (44423) - http://www.bohemiancoding.com/sketch -->
+					    <title>top-right</title>
+					    <desc>Created with Sketch.</desc>
+					    <defs>
+					        <circle id="path-1" cx="103.891459" cy="13.8914595" r="13.8914595"></circle>
+					        <filter x="-10.8%" y="-10.8%" width="121.6%" height="121.6%" filterUnits="objectBoundingBox" id="filter-2">
+					            <feGaussianBlur stdDeviation="1.5" in="SourceAlpha" result="shadowBlurInner1"></feGaussianBlur>
+					            <feOffset dx="0" dy="1" in="shadowBlurInner1" result="shadowOffsetInner1"></feOffset>
+					            <feComposite in="shadowOffsetInner1" in2="SourceAlpha" operator="arithmetic" k2="-1" k3="1" result="shadowInnerInner1"></feComposite>
+					            <feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.5 0" type="matrix" in="shadowInnerInner1"></feColorMatrix>
+					        </filter>
+					        <circle id="path-3" cx="27.4987181" cy="27.4987181" r="27.4987181"></circle>
+					    </defs>
+					    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+					        <g id="desk-homepage" transform="translate(-1030.000000, -59.000000)">
+					            <g id="corners" transform="translate(42.000000, 41.000000)">
+					                <g id="metals">
+					                    <g id="top-right" transform="translate(988.000000, 19.000000)">
+					                        <g id="tp-rght">
+					                            <use fill="#0F3E3F" fill-rule="evenodd" xlink:href="#path-1"></use>
+					                            <use fill="black" fill-opacity="1" filter="url(#filter-2)" xlink:href="#path-1"></use>
+					                            <use stroke="#979797" stroke-width="2" xlink:href="#path-1"></use>
+					                        </g>
+					                        <g id="light" transform="translate(0.000000, 33.000000)">
+					                            <g id="Oval-5">
+					                                <use fill="#EEEAEA" fill-rule="evenodd" xlink:href="#path-3"></use>
+					                                <circle class="hey" stroke="#C3C3C3" stroke-width="3" cx="27.4987181" cy="27.4987181" r="25.9987181"></circle>
+					                            </g>
+					                            <path d="M20.3490514,7.69964106 C12.0466221,11.9081817 7.2801776,17.0412757 6.04971797,23.0989232 C5.49974361,28.5986668 5.49974361,35.1983591 13.1993847,42.8980002 C20.8990257,50.5976412 35.1118598,47.7896741 39.598154,45.0978976 C39.0481796,40.1481284 37.3982566,30.7985642 25.848795,25.848795 C18.1491539,22.5489488 16.315906,16.4992308 20.3490514,7.69964106 Z" id="Path-6" fill-opacity="0.390000015" fill="#B3B3B3"></path>
+					                            <path d="M39.3830313,33.6051741 C39.7332657,35.4783853 39.5800522,37.0716523 38.9233908,38.3849751 C38.1748358,39.8820851 37.7240019,40.7710694 37.5708891,41.0519281 L38.0449725,45.9201799 L38.0448282,45.9201939 C38.0555419,46.0302106 38.1534132,46.1107115 38.2634298,46.0999978 C38.3287072,46.0936409 38.3867281,46.0557051 38.4187312,45.9984569 C40.5434683,42.1976678 41.5393295,39.6322009 41.4063151,38.3020563 C41.2808827,37.0477325 40.6743672,35.4373694 39.5867685,33.4709669 L39.3765788,33.5872208 C39.3796765,33.5928214 39.381855,33.598883 39.3830313,33.6051741 Z" id="Path-4" fill="#FFFFFF" opacity="0.735657649" transform="translate(39.494479, 40.179207) rotate(13.000000) translate(-39.494479, -40.179207) "></path>
+					                            <path d="M43.1039009,22.5633481 C42.7530595,23.9498113 42.3694424,25.1943077 41.9530496,26.2968371 C41.4859795,27.5335506 41.0325792,28.0145203 40.5928486,27.7397461 L44.9864737,37.9812662 L44.986487,37.9812604 C45.0300318,38.0827632 45.1476161,38.1297474 45.2491189,38.0862026 C45.2965397,38.065859 45.3342619,38.0279734 45.3544002,37.9804651 C47.3720351,33.2206717 48.3131871,30.1641212 48.1778564,28.8108138 C48.0441353,27.4736034 46.5205705,25.34502 43.6071619,22.4250636 L43.6072042,22.4250213 C43.4901552,22.3077092 43.3001679,22.307496 43.1828558,22.424545 C43.1444831,22.4628317 43.1171986,22.510798 43.1039009,22.5633481 Z" id="Path-4-Copy" fill="#FFFFFF" opacity="0.735657649" transform="translate(44.391732, 30.219678) rotate(13.000000) translate(-44.391732, -30.219678) "></path>
+					                        </g>
+					                    </g>
+					                </g>
+					            </g>
+					        </g>
+					    </g>
+					</svg>
+
+					<object class="metals bottom-right" data="wp-content/themes/bones/library/images/bottom-right.svg" type="image/svg+xml">
+					</object>
+
+					<object class="metals bottom-left" data="wp-content/themes/bones/library/images/btm-left.svg" type="image/svg+xml">
+					</object>
 
 				<div id="inner-content" class="wrap cf">
 
@@ -85,16 +139,29 @@
 
 							</article>
 
+						<div class="but-nav">
+						
+						<a href="http://vlstewart.com/idm250/wordpress/media/" style="display: block;">
+							<object class="but-press" data="wp-content/themes/bones/library/images/port.svg" type="image/svg+xml">
+							</object>
+						</a>
 
-							<object data="wp-content/themes/bones/library/images/port.svg" type="image/svg+xml">
+						<a href="http://vlstewart.com/idm250/wordpress/me/" style="display: block;">
+							<object class="but-press" data="wp-content/themes/bones/library/images/about.svg" type="image/svg+xml">
 							</object>
-							<object data="wp-content/themes/bones/library/images/about.svg" type="image/svg+xml">
-							</object>
-							<object data="wp-content/themes/bones/library/images/contact.svg" type="image/svg+xml">
-							</object>
-							<object data="wp-content/themes/bones/library/images/works.svg" type="image/svg+xml">
-							</object>
+						</a>
 
+						<a href="http://vlstewart.com/idm250/wordpress/contact/" style="display: block;">
+							<object class="but-press" data="wp-content/themes/bones/library/images/contact.svg" type="image/svg+xml">
+							</object>
+						</a>
+
+						<a href="http://vlstewart.com/idm250/wordpress/in-the-works/" style="display: block;">
+							<object class="but-press" data="wp-content/themes/bones/library/images/works.svg" type="image/svg+xml">
+							</object>
+						</a>
+
+						</div>
 							<?php endwhile; else : ?>
 
 									<article id="post-not-found" class="hentry cf">
@@ -119,5 +186,21 @@
 
 			</div>
 
+			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
-<?php get_footer(); ?>
+				<div id="inner-footer" class="wrap cf">
+
+				
+
+				</div>
+
+			</footer>
+
+		</div>
+
+		<?php // all js scripts are loaded in library/bones.php ?>
+		<?php wp_footer(); ?>
+
+	</body>
+
+</html> <!-- end of site. what a ride! -->
